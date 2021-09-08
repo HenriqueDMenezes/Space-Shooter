6faa534a193b242121d2e7161da0c7100348ec95
HenriqueDMenezes <henriquebm16@hotmail.com>
2021-09-08 19:50:31 -0300

    Função do tiro de laser

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function flyAhip(event){
   if(event.key== 'ArrowUp'){
     event.preventDefault();
     moveUp();
-  }else if (event.key=='ArrowDoen'){
+  }else if (event.key=='ArrowDown'){
     event.preventDefault();
     moveDown();
   }else if(event.key===" "){
@@ -17,14 +17,50 @@ function flyAhip(event){
 }
 
 //Função de subir
-
 function moveUp(){
   let topPosition = getComputedStyle(suaNave).getPropertyValue('top');
   if(topPosition==="0px"){
-    return
+    return;
   }else{
-    let postition = parseInt(topPosition)
+    let postition = parseInt(topPosition);
     postition -= 50;
     suaNave.style.top = `${postition}px`
   }
 }
+
+//função de descer
+function moveDown(){
+  let topPosition = getComputedStyle(suaNave).getPropertyValue('top');
+  if(topPosition==="500px"){
+    return;
+  }else{
+    let position = parseInt(topPosition)
+    position +=50;
+    suaNave.style.top = `${position}px`
+  }
+}
+
+//Funções de tiro
+function fireLaser(){
+  let laser = createLaserELement();
+  playArea.appendChild(laser)
+  moveLaser();
+}
+
+function createLaserELement(){
+  let xPosition = parseInt(window.getComputedStyle(suaNave).getPropertyValue('left'));
+  let yPosition = parseInt(window.getComputedStyle(suaNave).getPropertyValue('top'));
+  let newLaser = document.createElement('img');
+  newLaser.scr = 'img/shoot.png';
+  newLaser.classList.add('laser');
+  newLaser.style.left = `${xPosition}px`;
+  newLaser.style.top = `${yPosition -10}px`;
+  return newLaser;
+}
+
+
+function moveLaser(){
+
+}
+
+window.addEventListener('keydown',flyAhip)
